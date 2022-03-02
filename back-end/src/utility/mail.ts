1bca7639224448d810503b82e642cbe6b8402cd6
Billie Zhang <billiecoder@gmail.com>
2022-03-02 21:49:21 +0800

    replace mailgun with nodemailer

--- back-end/src/utility/mail.ts
+++ back-end/src/utility/mail.ts
@@ -1,60 +1,39 @@
-import * as Mailgun from 'mailgun-js'
 import * as _ from 'lodash'
+import nodemailer from 'nodemailer'
 
 export default {
+  async send(options) {
+    const { to, toName, subject, body } = options
 
-    /**
-     *
-     * @param {any} to
-     * @param {any} toName
-     * @param {any} subject
-     * @param {any} body
-     * @param {any} replyTo - {name, email}
-     * @param {any} recVariables - multiple receipent metadata
-     * @returns {Promise<any>}
-     */
-    async send(options) {
-        const {to, toName, subject, body, replyTo, recVariables} = options
-
-        if (!process.env.MAILGUN_API_KEY || !process.env.MAILGUN_URL) {
-            return
-        }
-
-        const mailgun = Mailgun({
-            apiKey: process.env.MAILGUN_API_KEY,
-            domain: process.env.MAILGUN_URL
-        })
-
-        const data = {
-            from: 'Cyber Republic - Elastos <no-reply@elastosjs.com>',
-            to: _.isArray(to) ? to : `${toName} <${to}>`,
-            subject: subject,
-            html: body,
-            'recipient-variables': _.isArray(to) && recVariables
-        }
-
-        if (replyTo && !_.isEmpty(replyTo)) {
-            data['h:Reply-To'] = `${replyTo.name} <${replyTo.email}>`
-        }
-
-        if (process.env.NODE_ENV === 'dev') {
-            console.log('Debug - Sending Mail:', data)
-        }
-
-        return new Promise((resolve, reject) => {
-
-            resolve()
-
-            mailgun.messages().send(data, function (err, body) {
-                if (err) {
-                    console.error(err)
-                    reject(err)
-                    return
-                }
-                // console.log(body);
-                resolve()
-            })
-        })
-
+    const data = {
+      from: 'Cyber Republic - Elastos <no-reply@elastosjs.com>',
+      to: _.isArray(to) ? to : `${toName} <${to}>`,
+      subject: subject,
+      html: body
     }
+
+    if (process.env.NODE_ENV === 'dev') {
+      console.log('Debug - Sending Mail:', data)
+    }
+
+    let testAccount = await nodemailer.createTestAccount()
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: testAccount.user, // generated ethereal user
+        pass: testAccount.pass // generated ethereal password
+      }
+    })
+
+    // send mail with defined transport object
+    const info = await transporter.sendMail(data)
+
+    console.log('Message sent: %s', info.messageId)
+    // Preview only available when sending through an Ethereal account
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info))
+  }
 }
