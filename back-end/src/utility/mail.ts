2f0a17453bd0d347c492ab6c5e8c63ef0a799820
Billie Zhang <billiecoder@gmail.com>
2022-03-03 10:57:18 +0800

    config env to send real mail

--- back-end/src/utility/mail.ts
+++ back-end/src/utility/mail.ts
@@ -1,39 +1,34 @@
 import * as _ from 'lodash'
 import * as nodemailer from 'nodemailer'
 
+// create reusable transporter object using the default SMTP transport
+const transporter = nodemailer.createTransport({
+  host: process.env.NODEMAILER_HOST,
+  port: 465,
+  secure: true, // true for 465, false for other ports
+  auth: {
+    user: process.env.NODEMAILER_USER, // generated ethereal user
+    pass: process.env.NODEMAILER_PASS // generated ethereal password
+  }
+})
+
 export default {
   async send(options) {
-    const { to, toName, subject, body } = options
-
-    const data = {
-      from: 'Cyber Republic - Elastos <no-reply@cyberrepublic.org>',
-      to: _.isArray(to) ? to : `${toName} <${to}>`,
-      subject: subject,
-      html: body
-    }
-
-    if (process.env.NODE_ENV === 'dev') {
-      console.log('Debug - Sending Mail:', data)
-    }
-
-    let testAccount = await nodemailer.createTestAccount()
-
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-      host: 'smtp.ethereal.email',
-      port: 587,
-      secure: false, // true for 465, false for other ports
-      auth: {
-        user: testAccount.user, // generated ethereal user
-        pass: testAccount.pass // generated ethereal password
+    try {
+      const { to, toName, subject, body } = options
+      const data = {
+        from: 'Cyber Republic - Elastos <no-reply@cyberrepublic.org>',
+        to: _.isArray(to) ? to : `${toName} <${to}>`,
+        subject: subject,
+        html: body
       }
-    })
-
-    // send mail with defined transport object
-    const info = await transporter.sendMail(data)
-
-    console.log('Message sent: %s', info.messageId)
-    // Preview only available when sending through an Ethereal account
-    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info))
+      if (process.env.NODE_ENV === 'dev') {
+        console.log('Debug - Sending Mail:', data)
+      }
+      // send mail with defined transport object
+      await transporter.sendMail(data)
+    } catch (err) {
+      console.log('nodemiler sending email err...', err)
+    }
   }
 }
